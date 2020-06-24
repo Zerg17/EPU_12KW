0ed31d5beb56bf80b0ee5ba3908c982c214a76e4
Zerg17 <zerg1703@yandex.ru>
2020-06-24 16:04:19 +0300

    Начал делать ETH

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "system.h"
+#include "lan8742.h"
 
 int main(void){
-    
     sysInit();
 
     while(1){
--- src/system.c
+++ src/system.c
@@ -1,20 +1,47 @@
 #include "system.h"
 
 void rccInit(){
-    RCC->AHB4ENR |= RCC_AHB4ENR_GPIOBEN | RCC_AHB4ENR_GPIODEN | RCC_AHB4ENR_GPIOEEN;
+    RCC->AHB4ENR |= RCC_AHB4ENR_GPIOAEN | RCC_AHB4ENR_GPIOBEN | RCC_AHB4ENR_GPIOCEN 
+                 |  RCC_AHB4ENR_GPIODEN | RCC_AHB4ENR_GPIOEEN | RCC_AHB4ENR_GPIOGEN
+                 |  RCC_APB4ENR_SYSCFGEN| ;
     RCC->APB1LENR |= RCC_APB1LENR_USART3EN;
 }
 
+void sysCfgInit(){
+    SYSCFG->PMCR |= SYSCFG_PMCR_EPIS_SEL_2;
+}
+
 void gpioInit(){
-    GPIOB->MODER &= ~(GPIO_MODER_MODER0_1);  // LED GREEN
-    GPIOE->MODER &= ~(GPIO_MODER_MODER1_1);  // LED YELLOW
-    GPIOB->MODER &= ~(GPIO_MODER_MODER14_1); // LED RED
+    // LEDS
+    GPIOB->MODER &= ~(GPIO_MODER_MODER0_1);     // LED GREEN
+    GPIOE->MODER &= ~(GPIO_MODER_MODER1_1);     // LED YELLOW
+    GPIOB->MODER &= ~(GPIO_MODER_MODER14_1);    // LED RED
 
-    GPIOD->MODER &= ~(GPIO_MODER_MODER8_0); // USART3 TX
-    GPIOD->AFR[1] |= 0x00000007;
+    // USART3
+    GPIOD->MODER &= ~(GPIO_MODER_MODER8_0);     // USART3 TX
+    GPIOD->AFR[1] |= 7<<4*0;
+    GPIOD->MODER &= ~(GPIO_MODER_MODER9_0);     // USART3 TX
+    GPIOD->AFR[1] |= 7<<4*1;
 
-    GPIOD->MODER &= ~(GPIO_MODER_MODER9_0); // USART3 TX
-    GPIOD->AFR[1] |= 0x00000070;
+    // ETH
+    GPIOA->MODER &= ~(GPIO_MODER_MODER1_0);     // ETH_REF_CLK
+    GPIOA->AFR[0] |= 11<<4*1;
+    GPIOA->MODER &= ~(GPIO_MODER_MODER2_0);     // ETH_MDIO
+    GPIOA->AFR[0] |= 11<<4*2;
+    GPIOA->MODER &= ~(GPIO_MODER_MODER7_0);     // ETH_CRS_DV
+    GPIOA->AFR[0] |= 11<<4*7;
+    GPIOB->MODER &= ~(GPIO_MODER_MODER13_0);    // ETH_TXD1
+    GPIOB->AFR[1] |= 11<<4*5;
+    GPIOC->MODER &= ~(GPIO_MODER_MODER1_0);     // ETH_MDC
+    GPIOC->AFR[0] |= 11<<4*1;
+    GPIOC->MODER &= ~(GPIO_MODER_MODER4_0);     // ETH_RXD0
+    GPIOC->AFR[0] |= 11<<4*4;
+    GPIOC->MODER &= ~(GPIO_MODER_MODER5_0);     // ETH_RXD1
+    GPIOC->AFR[0] |= 11<<4*5;
+    GPIOG->MODER &= ~(GPIO_MODER_MODER11_0);    // ETH_TX_EX
+    GPIOG->AFR[1] |= 11<<4*3;
+    GPIOG->MODER &= ~(GPIO_MODER_MODER13_0);    // ETH_TXD0
+    GPIOG->AFR[1] |= 11<<4*5;
 }
 
 void uartInit(){
@@ -29,9 +56,15 @@ void uartWrite(uint8_t d){
     USART3->TDR=d;
 }
 
+void ethInit(){
+    ETH->DMAMR |= ETH_DMAMR_SWR;
+    while(ETH->DMAMR & ETH_DMAMR_SWR);
+}
+
 void sysInit(){
     xdev_out(uartWrite);
     rccInit();
+    sysCfgInit();
     delayInit();
     gpioInit();
     uartInit();
